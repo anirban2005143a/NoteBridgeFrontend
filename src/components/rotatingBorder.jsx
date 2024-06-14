2a47bdebd2f782b32251967696abdf3d9f17e357
anirban2005143a <23je0104@iitism.ac.in>
2024-06-14 16:15:20 +0530

    first commit

--- src/components/rotatingBorder.jsx
+++ src/components/rotatingBorder.jsx
@@ -3,7 +3,7 @@ import "../css/rotatingBorder.css"
 
 const rotatingBorder = (props) => {
   return (
-    <div className="emptyPost text-center mx-auto w-50 mt-4 px-3">
+    <div className="emptyPost text-center mx-auto w-50 mt-4 " >
     <h2>{props.message}</h2>
   </div>
   )
